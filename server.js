1392dff0dfec5b33223ac068f56bcc8ae9ae0ad9
arsci <russerya@gmail.com>
2016-07-20 09:19:01 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.post('', function(req, res) {
+app.get('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issue
 		conn.query('SELECT id, sensorValue__c, sensorTimestamp__c FROM salesforce.sensorData__c ORDER BY id DESC LIMIT 1',
