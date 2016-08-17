25d741043867c622067949c604eb215ae2e043bf
arsci2 <russerya+unsec2@gmail.com>
2016-08-17 08:39:49 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var pg = require('pg');
-
 var app = express();
-
 app.set('port', process.env.PORT || 5000);
-
 app.use(express.static('public'));
 app.use(bodyParser.json());
-
 app.get('/update', function(req, res) {
-    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+	pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
 		conn.query('SELECT * FROM salesforce.sensorData__c ORDER BY id DESC LIMIT 20',
 			function(err, result) {
 				done();
@@ -18,13 +14,12 @@ app.get('/update', function(req, res) {
 					res.status(400).json({error: err.message});
 				}
 				else {
-            				res.json(result);
+					res.json(result);
 				}
 			}
 		);
 	});
 });
-
 app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });
